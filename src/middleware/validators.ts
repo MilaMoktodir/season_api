10aa3dce45c70f75b5cc224d3eba0db0e808a7ea
MilaMoktodir <160149601+MilaMoktodir@users.noreply.github.com>
2024-05-01 18:41:26 +0000

    chnage

--- src/middleware/validators.ts
+++ src/middleware/validators.ts
@@ -13,7 +13,7 @@ import { param } from "express-validator";
 export const validateCityName = param("city")
   // We will use the isString method to check if the city param is a string
   .isString()
-  // We will use the isIn method to check if the city param is either london or dublin
-  .isIn(["london", "dublin"])
+  // We will use the isIn method to check if the city param is either london or dublin or tokyo
+  .isIn(["london", "dublin", "tokyo"])
   // We will use the withMessage method to set a custom error message
-  .withMessage("City name must be either london or dublin");
+  .withMessage("City name must be either london dublin, or tokyo");
